465b2a9aa72085b55bb526de7525362879fa9bdc
Sadık Orak <muhammedsadikorak@gmail.com>
2025-02-28 02:00:40 +0300

    tabs projesi react a deneme olarak eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+const dataPath = "../public/json/data.json"
+import React, { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,10 +8,54 @@ import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState()
+
+  useEffect(() => {
+
+    async function getData() {
+      try {
+
+        const response = await fetch(dataPath)
+        const resData = await response.json()
+        setData(resData)
+
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    getData();
+
+  }, [])
 
   return (
     <>
+      <div className='github'>
+        <h1>GitHub</h1>
+        {
+          data?.homeworks?.map(d => (
+            <div className='card' key={d.projectName}>
+              <a href={d.github} target='_blank'>
+                {d.projectName}
+              </a>
+            </div>
+          ))
+        }
+
+      </div>
+
+      <div className='vercel'>
+        <h1>Vercel</h1>
+        {
+          data?.homeworks?.map(d => (
+            <div className='card' key={d.projectName}>
+              <a href={d.vercel} target='_blank'>
+                {d.projectName}
+              </a>
+            </div>
+          ))
+        }
+      </div>
 
     </>
   )
